c07f60ae496cf9463e6f7d7fe178c8cb3f3473af
Asus <Asus@ASUS>
2024-10-14 15:11:53 +0700

    Add project files.

---
+++ MaSV_MyStory/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MaSV_MyStory.Models;
+
+namespace MaSV_MyStory.Areas.Admin.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private MyStoreEntities db = new MyStoreEntities();
+
+        // GET: Admin/Categories
+        // GET: Lấy dữ liệu từ bảng category trong DB để hiển thị lên
+        public ActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+
+        // GET: Admin/Categories/Details/5
+        // details : Lấy chi tiết một bản ghi có CategoryID = id
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);//mã lỗi 400: thiếu giá trị truyền vạo
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)//không tìm thấy bản ghi
+            {
+                return HttpNotFound();//mã lỗi 404
+            }
+            return View(category);
+        }
+
+        // GET: Admin/Categories/Create
+        // Load form Create
+        //[HttpGet] là phương thức mặc định, nên không cần khai báo từ khóa
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Categories/Create
+        // POST: Lưu dữ liệu nhập vào từ from Create vào DB
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CategoryID,CategoryName")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        // GET: Admin/Categories/Edit/5
+        // GET: Lấy dữ liệu của một danh mục đã có sao cho CategoryID = id
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Admin/Categories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CategoryID,CategoryName")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Admin/Categories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Admin/Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MaSV_MyStory/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MaSV_MyStory.MvcApplication" Language="C#" %>
